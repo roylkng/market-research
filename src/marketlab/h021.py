3b97d5dffccec37a8c23a3864d551cd15ca23d07
roylkng <rajan.kadeval@gmail.com>
2026-09-11 02:18:35 +0530

    H021: fix import-block lint

--- src/marketlab/h021.py
+++ src/marketlab/h021.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from dataclasses import dataclass
 from datetime import UTC, date, datetime
 
-
 REQUIRED_OBSERVATION_FIELDS = {
     "symbol",
     "fiscal_period",
